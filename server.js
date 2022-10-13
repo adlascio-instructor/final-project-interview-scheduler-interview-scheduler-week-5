31c6a058937dc2df85bab3f3bb9a802c6304f336
Nicolas <nickismeza@gmail.com>
2022-10-13 14:52:31 -0700

    checking api

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const apiRoute=require('./backend/server-api/routes/interviewsRoute')
+// const io=require('socket.io')(3000)
+
+// io.on("connection",socket=>{
+//     console.log(socket.id)
+// })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
