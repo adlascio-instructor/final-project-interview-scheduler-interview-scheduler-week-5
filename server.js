fe9d880bc74f99cb771b6ea30cb36941165a849f
Nicolas <nickismeza@gmail.com>
2022-10-13 22:35:46 -0700

    working sockets

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const app = express();
 const port = 8000;
-const interviewerRoute=require('./backend/server-api/routes/interviewerRoute')
 const interviewsRoute=require('./backend/server-api/routes/interviewsRoute')
+const cors = require('cors');
+// Require for the socket.io
+const http=require('http')
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(cors({
+    origin:"*",
+    credentials:true,
+}
+))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
-app.use('/',interviewerRoute)
-app.use('/',interviewsRoute)
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.use('/interviews',interviewsRoute);
 
+server.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// Web sockets
+
+io.on("connection",socket=>{
+    console.log(socket.id)
+})
