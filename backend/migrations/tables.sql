0b4d8f82f4cd562e5337cf32807cf81abe00099d
Nicolas <nickismeza@gmail.com>
2022-10-18 16:55:33 -0700

    almos done

--- backend/migrations/tables.sql
+++ backend/migrations/tables.sql
@@ -1,31 +1,31 @@
 --tables
 CREATE TABLE Appointments(
-    id INTEGER PRIMARY KEY NOT NULL,       --: Unique value for each appointment
+    id serial PRIMARY KEY,       --: Unique value for each appointment
     time VARCHAR(20) not null,            --: Time of the appointment (You can set as string)
     day_id integer not null                 --: Id of the day of the appointment
-) 
+);
 
 CREATE TABLE Day(
-    id   integer primary key not null,    --: Unique value for each day
+    id   serial primary key not null,    --: Unique value for each day
     name  varchar(20) 	                  --: Name of the day (Monday to Friday)
-)	
+);
 	
 CREATE TABLE Interviews(
-    id  integer primary key not null, --: Unique value for each interview
+    id  serial primary key , --: Unique value for each interview
     student  varchar(100) not null,   --: Name of the student
     interviewer_id  integer not null, --: Id of the interviewer
     appointment_id  integer not null  --: Id of the appointment
-)
+);
 	
 CREATE TABLE Interviewers(
-    id integer primary key not null, --: Unique value for each interviewer
+    id serial primary key , --: Unique value for each interviewer
     name    varchar(100) not null,   --: Name of the interviewer
     avatar  varchar(150) null        --: Url of the interviewer's avatar
-)
+);
 	
 	
 CREATE TABLE Available_Interviewers(
-    id  integer primary key not null,  --: Unique value for each available interviewer
+    id  serial primary key ,  --: Unique value for each available interviewer
     interviewer_id   integer not null, --: Id of the interviewer
     day_id  integer not null          --: Id of the DAY
-)	
+);
